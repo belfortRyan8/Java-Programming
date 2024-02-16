3b32d1d88c482483cd1430d3eea7b311cb91b66e
ripan baidya <ripanbaidya7980@gmail.com>
2024-02-16 14:02:33 +0530

    first commit

---
+++ collectionAndFramework/ArrayListExample.java
@@ -0,0 +1,13 @@
+package collectionAndFramework;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        System.out.println(al);
+    }
+}
