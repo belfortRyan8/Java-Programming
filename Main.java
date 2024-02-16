c21d95cc24bda717c5d6cfa53191aa62fcc7b50e
ripan baidya <ripanbaidya7980@gmail.com>
2024-02-16 13:59:52 +0530

    commit added

--- Main.java
+++ Main.java
@@ -2,15 +2,7 @@ import  java.lang.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String... args){
-        System.out.println(m1());
-    }
-    public static int m1(){
-        try{
-            System.out.println(10/0);
-            return 777;
-        }catch(ArithmeticException e){
-            return 888;
-        }
+    public static void main(String[] args) {
+
     }
 }
