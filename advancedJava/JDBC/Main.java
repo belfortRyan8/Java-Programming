4d4cd0aa3e3353926b1fb322ec5930f5704c37c1
ripan baidya <ripanbaidya7980@gmail.com>
2024-02-18 03:07:47 +0530

    second commit

--- advancedJava/JDBC/Main.java
+++ advancedJava/JDBC/Main.java
@@ -1,7 +1,44 @@
 package advancedJava.JDBC;
 
-public class Main {
+import java.lang.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println("jdbc");
+        String url = "jdbc:mysql://localhost:3306/employeeDatabase";
+        String username = "Username";
+        String password = "Password";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employeeDatabase");
+
+            // Process the result set and print the retrieved data
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+
+                System.out.println("ID: " + id + ", Name: " + name + ", Age: " + age);
+            }
+
+            // Close the result set, statement, and connection
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("SQL Exception occurred!");
+            e.printStackTrace();
+        }
     }
 }
